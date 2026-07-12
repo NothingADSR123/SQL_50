5a0bde83b967a0d8db4066fb3b9ea457b57e3967
nothingADSR123 <nothingADSR123@gmail.com>
2026-07-12 11:06:47 +0530

    SQL_Joins

--- solutions/1148.ArticleViewsI.sql
+++ solutions/1148.ArticleViewsI.sql
@@ -47,3 +47,10 @@
 
 
 select distinct author_id as id from Views where author_id=viewer_id order by id
+
+
+SELECT author_id AS id 
+FROM Views 
+WHERE author_id = viewer_id 
+GROUP BY author_id 
+ORDER BY author_id;
